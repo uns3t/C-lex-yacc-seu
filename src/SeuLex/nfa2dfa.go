2723642bcf434b2159a66d6349d3f9efbb2f5424
uns3t <uns3tzhang@gmail.com>
2020-05-18 00:53:28 +0800

    基本完成，明天测试一下

--- src/SeuLex/nfa2dfa.go
+++ src/SeuLex/nfa2dfa.go
@@ -25,10 +25,10 @@ func searchClosure(Nnode NState) []NState{
 		back=back[:len(back)]
 		ret=append(ret,temp)
 		if temp.c>255{
-			if(temp.out1!=nil){
+			if temp.out1!=nil{
 				back=append(back,temp.out1)
 			}
-			if(temp.out2!=nil){
+			if temp.out2!=nil{
 				back=append(back,temp.out2)
 			}
 		}
@@ -38,7 +38,7 @@ func searchClosure(Nnode NState) []NState{
 
 func retDnodeOut(Narr []NState)  {
 	for i:=0;i<len(Narr);i++{
-		if(Narr[i].c<=255){
+		if Narr[i].c<=255{
 			backLook=append(backLook,Narr[i])
 		}
 	}
@@ -50,20 +50,22 @@ type Dtemp struct {
 }
 
 
-func nfa2dfa(nfa NState){
+func nfa2dfa(nfa NState) []Dnode{
 	dtemp:=[]Dtemp{}
 	backLook=append(backLook,nfa)
+	dArr:=[]Dnode{}
 	for len(backLook)>0{
 		tempNode:=backLook[0]
 		backLook=backLook[1:]
 		flag:=false
 		for i:=0;i<len(dtemp);i++{
-			if(dtemp[i].Nnode==tempNode){
+			if dtemp[i].Nnode==tempNode{
 				flag=true
 			}
 		}
-		if(!flag){
+		if !flag{
 			tempClosure:=searchClosure(tempNode)
+			dArr= append(dArr, Dnode{tempClosure,[]Dstate{}})
 			Onedtemp:=Dtemp{tempNode,tempClosure}
 			dtemp=append(dtemp,Onedtemp)
 			for i:=0;i<len(tempClosure);i++{
@@ -72,7 +74,20 @@ func nfa2dfa(nfa NState){
 				}
 			}
 		}
-
 	}
-
+	for i:=0;i<len(dArr);i++{
+		for j:=0;j<len(dArr[i].Nnodes);j++{
+			if(dArr[i].Nnodes[j].c<=255){
+				idx:=0
+				for true{
+					if dtemp[idx].Nnode==dArr[i].Nnodes[j]{
+						dArr[i].Dout=append(dArr[i].Dout,Dstate{c,dArr[idx]})
+						break
+					}
+					idx++
+				}
+			}
+		}
+	}
+	return dArr
 }
