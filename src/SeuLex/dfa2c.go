a45aa7d19eca3da85d9d66f687c507abee7d6d03
uns3t <uns3tzhang@gmail.com>
2020-05-18 14:17:28 +0800

    dfa2c初步

--- src/SeuLex/dfa2c.go
+++ src/SeuLex/dfa2c.go
@@ -1,1 +1,48 @@
 package SeuLex
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func Dfa2c(dArr []Dnode)  {
+	lexFile,_:=os.Create("lex.c")
+
+	fullOutput := `
+  
+    int cp = 0;
+    int state = 0;
+    
+    void dfa(char c);
+    
+    ${dfa.userDefineProgram}
+
+    int main(int argc, const char * argv[]) {
+		fp = fopen(argv[1], "r");
+        while (1) {
+			if(feof(fp)) break;
+			char buf;
+			fscanf(fp,"%c",buf)
+            try {
+                dfa(buf);
+            } catch(int e) {
+                printf("出现错误")
+                break;
+            }
+        }
+        return 0;
+    }
+    
+    void dfa(char c){
+        switch(state){
+            ${coreOutput.join('\n')}
+            default:
+                throw 1;
+                break;
+        }
+    }
+    `
+	lexFile.WriteString(fullOutput)
+	fmt.Println("写入lex.c完成")
+}
