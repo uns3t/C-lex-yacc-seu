541d4e981bec2cd49f48934e6add2ee8389bf3ec
uns3t <uns3tzhang@gmail.com>
2020-05-28 18:02:56 +0800

    yacc解析基本完成

--- src/SeuYacc/loader.go
+++ src/SeuYacc/loader.go
@@ -8,15 +8,17 @@ import (
 
 type YaccFile struct {
 	DefineSegment defineSegment
-	GrammarSegment grammarSegment
+	GrammarSegment []grammarSegment
 	ProgramSegment string
 }
 type defineSegment struct {
-
+	Token []string
+	Start string
 }
 
 type grammarSegment struct {
-
+	leftPart string
+	rightPart [][]string
 }
 
 func LoadYaccFile(yaccPath string) YaccFile {
@@ -33,9 +35,41 @@ func LoadYaccFile(yaccPath string) YaccFile {
 }
 
 func parseDefineSegment(str string)  defineSegment{
-	
+	arr:=strings.Split("\r\n",str)
+	ret:=defineSegment{[]string{},""}
+	for _,v:=range arr{
+		if string(v[0])=="%"{
+			temp:=strings.Split(v," ")
+			if temp[0]=="%token"{
+				for i:=1;i<len(temp);i++{
+					ret.Token=append(ret.Token,temp[i])
+				}
+			}else if temp[0]=="%start"{
+				ret.Start=temp[1]
+			}
+		}
+	}
+	return ret
 }
 
-func parseGrammarSegment(str string)  grammarSegment{
-	
+func parseGrammarSegment(str string)  []grammarSegment{
+	arr:=strings.Split("\r\n;",str)
+	newArr:=make([]string, len(arr))
+	ret:=make([]grammarSegment, len(arr))
+	for k,_:=range arr{
+		arr[k]=strings.Trim(arr[k]," ")
+		if len(arr[k])>0{
+			newArr=append(newArr,arr[k])
+		}
+	}
+	for _,v:=range newArr{
+		temp:=strings.Split(v,":")
+		tempRight:=strings.Split(strings.Trim(temp[1]," "),"|")
+		right:=make([][]string,len(tempRight))
+		for _,v:=range tempRight{
+			right=append(right,strings.Split(strings.Trim(v," ")," "))
+		}
+		ret=append(ret,grammarSegment{strings.Trim(temp[0]," "),right})
+	}
+	return ret
 }
