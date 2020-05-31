b6f0d51f02863823264adf672ff2c877331fcdb9
SoakedThrough <32643629+SoakedThrough@users.noreply.github.com>
2020-05-31 23:21:09 +0800

    初步完成lr1.go
    还未测试

--- src/SeuYacc/lr1.go
+++ src/SeuYacc/lr1.go
@@ -1,82 +1,131 @@
 package SeuYacc
+
+import (
+	"sort"
+	"strings"
+)
+
+//lr1语法分析
+
+//语法解析, 状态构建, 状态去重, 状态转移, Action表与Goto表的构建
 //
-//import (
-//	"sort"
-//	"strings"
-//)
-//
-////lr1语法分析
-//
-////语法解析, 状态构建, 状态去重, 状态转移, Action表与Goto表的构建
-////
-//
-//
-//func generateLR1DFA(I0 *lrState,grammar *grammar)  {
-//	I0 = expandDFAItem(I0,grammar)
-//}
-//
-//func expandDFAItem(dfaItem *lrState, grammar *grammar) *lrState {
-//	expandFlag := true
-//	for expandFlag {
-//		expandFlag = false
-//		for itemName := range dfaItem.items {
-//			item := dfaItem.items[itemName]
-//			position := item.position
-//			pCode := strings.Split(itemName,"-")[0]
-//			predictor := item.predictor
-//			rightPart := getRightPart(grammar,pCode)
-//			if isVn(grammar,rightPart[position]) {
-//				// 点的后面是非终结符
-//				newPredictor := make(map[string]bool)
-//				var newKey []string
-//				// 先计算新的预测符
-//				for vt := range predictor {
-//					betaAlpha := append(rightPart[position+1:],predictor[vt])
-//					first := first(betaAlpha,grammar)
-//					for vtt := range first {
-//						newPredictor[first[vtt]] = true
-//						newKey = append(newKey, first[vtt])
-//					}
-//				}
-//				sort.Strings(newKey)
-//
-//				creator := p(grammar,rightPart[position])
-//				// 遍历该非终结符为左部的所有的产生式
-//				for p := range creator {
-//					name := getPCode(creator[p]) + "-0"
-//					if dfaItem.items[name] != nil {
-//						// 如果已经包含该产生式，检查是否需要扩展预测符
-//						if strings.Join(newKey,"|") != strings.Join(dfaItem.items[name].predictor,"|") {
-//							// 新的预测符和旧的不相等，判断是否属于包含关系
-//							old := strings.Join(dfaItem.items[name].predictor,"｜")
-//							for i := range newKey {
-//								if !strings.Contains(old,newKey[i]) {
-//									dfaItem.items[name].predictor = append(dfaItem.items[name].predictor, newKey[i])
-//									expandFlag = true// 有新的预测符加入
-//								}
-//							}
-//							if expandFlag {
-//								sort.Strings(dfaItem.items[name].predictor)
-//							}
-//						}
-//					}else {
-//						dfaItem.items[name] = NewItemp(newKey,0,strings.Join(
-//							getRightPart(grammar,getPCode(creator[p])),""))
-//						expandFlag = true
-//					}
-//				}
-//			}
-//		}
-//	}
-//	// 对 dfaItem 计算签名，用于后续判断是否出现重复状态
-//	// signature 是完整签名
-//
-//	/*
-//	??????????????
-//	 */
-//	return dfaItem
-//}
-//
-//func getOutEdge()  {
-//
-//}
+func getOutEdge(dfaItem *lrState) []string {
+	var outEdge []string
+	for n := range dfaItem.items {
+		if dfaItem.items[n].position < len(dfaItem.items[n].rightPart) {
+			outEdge = append(outEdge, dfaItem.items[n].rightPart[dfaItem.items[n].position])
+		}
+	}
+	return outEdge
+}
+
+func nextStep(dfaItem *lrState, edge string) *lrState {
+	newDfaItem := NewLrState("")
+	for n := range dfaItem.items {
+		pCode := strings.Split(n,"-")[0]
+		if dfaItem.items[n].rightPart[dfaItem.items[n].position] == edge {
+			temp := NewItemp(dfaItem.items[n].predictor,dfaItem.items[n].position,
+				dfaItem.items[n].rightPart)
+			flag := pCode + "-" + string(dfaItem.items[n].position+1)
+			newDfaItem.items[flag] = temp
+		}
+	}
+	return newDfaItem
+}
+
+func generateLR1DFA(I0 *lrState,grammar *Grammar) map[string]*lrState {
+	lrDFA := make(map[string]*lrState)
+	lr1DfaSigniture := make(map[string]string)
+	dfaItemQ := NewDfaQueue()
+	nameCounter := 1
+
+	I0 = expandDFAItem(I0,grammar)
+	dfaItemQ.Push(I0)
+	lr1DfaSigniture[I0.signature] = "I0"
+	for len(dfaItemQ.lrStatequeue)>0 {
+		currentItem := dfaItemQ.Shift()
+		outEdges := getOutEdge(currentItem)
+		for i := range outEdges {
+			nextItem := nextStep(currentItem,outEdges[i])
+			nextItem = expandDFAItem(nextItem,grammar)
+			if lr1DfaSigniture[nextItem.signature] != "" {
+				nextItem.name = "I" + string(nameCounter+1)
+				lr1DfaSigniture[nextItem.signature] = nextItem.name
+				dfaItemQ.Push(nextItem)
+				currentItem.edge[outEdges[i]] = nextItem.name
+			}else {
+				currentItem.edge[outEdges[i]] = lr1DfaSigniture[nextItem.signature]
+			}
+		}
+		lrDFA[currentItem.name] = currentItem
+	}
+	return lrDFA
+}
+
+func expandDFAItem(dfaItem *lrState, grammar *Grammar) *lrState {
+	expandFlag := true
+	for expandFlag {
+		expandFlag = false
+		for itemName := range dfaItem.items {
+			item := dfaItem.items[itemName]
+			position := item.position
+			pCode := strings.Split(itemName,"-")[0]
+			predictor := item.predictor
+			rightPart := getRightPart(*grammar,pCode)
+			if isVn(*grammar,rightPart[position]) {
+				// 点的后面是非终结符
+				newPredictor := make(map[string]bool)
+				var newKey []string
+				// 先计算新的预测符
+				for vt := range predictor {
+					betaAlpha := append(rightPart[position+1:],predictor[vt])
+					first := first(betaAlpha,*grammar)
+					for vtt := range first {
+						newPredictor[first[vtt]] = true
+						newKey = append(newKey, first[vtt])
+					}
+				}
+				sort.Strings(newKey)
+
+				creator := grammar.p[rightPart[position]]
+				// 遍历该非终结符为左部的所有的产生式
+				for p := range creator {
+					name := creator[p].code + "-0"
+					if dfaItem.items[name] != nil {
+						// 如果已经包含该产生式，检查是否需要扩展预测符
+						if strings.Join(newKey,"|") != strings.Join(dfaItem.items[name].predictor,"|") {
+							// 新的预测符和旧的不相等，判断是否属于包含关系
+							old := strings.Join(dfaItem.items[name].predictor,"｜")
+							for i := range newKey {
+								if !strings.Contains(old,newKey[i]) {
+									dfaItem.items[name].predictor = append(dfaItem.items[name].predictor, newKey[i])
+									expandFlag = true// 有新的预测符加入
+								}
+							}
+							if expandFlag {
+								sort.Strings(dfaItem.items[name].predictor)
+							}
+						}
+					}else {
+						dfaItem.items[name] = NewItemp(newKey,0,
+							getRightPart(*grammar,creator[p].code))
+						expandFlag = true
+					}
+				}
+			}
+		}
+	}
+	// 对 dfaItem 计算签名，用于后续判断是否出现重复状态
+	// signature 是完整签名
+	var sig []string
+	for n := range dfaItem.items {
+		str := n + "-" + strings.Join(dfaItem.items[n].predictor,"|")
+		sig = append(sig, str)
+	}
+	sort.Strings(sig)
+	dfaItem.signature = strings.Join(sig,"•")
+	/*
+	??????????????
+	 */
+	return dfaItem
+}
--- src/SeuYacc/lrState.go
+++ src/SeuYacc/lrState.go
@@ -4,15 +4,39 @@ package SeuYacc
 type lrState struct {
 	name string
 	items map[string]*item
-
+	signature string
+	edge map[string]string
 }
 
 type item struct {
 	predictor []string
 	position int
-	rightPart string
+	rightPart []string
 }
 
-func NewItemp(pre []string, pos int, rightP string) *item {
+func NewLrState(n string) *lrState {
+	return &lrState{name: n,items: make(map[string]*item),edge: make(map[string]string)}
+}
+
+func NewItemp(pre []string, pos int, rightP []string) *item {
 	return &item{predictor: pre,position: pos,rightPart: rightP}
+}
+
+type dfaItemQueue struct {
+	lrStatequeue []*lrState
+}
+
+func NewDfaQueue() *dfaItemQueue {
+	return &dfaItemQueue{}
+}
+func(this *dfaItemQueue) Push(state *lrState)  {
+	this.lrStatequeue = append(this.lrStatequeue, state)
+}
+func(this *dfaItemQueue) Shift() *lrState {
+	if len(this.lrStatequeue) == 0 {
+		panic("dfa队列错误，为空")
+	}
+	temp := this.lrStatequeue[0]
+	this.lrStatequeue = this.lrStatequeue[1:]
+	return temp
 }
